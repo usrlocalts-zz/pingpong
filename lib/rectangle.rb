b0acd6aa0a6d31608d6d0888dc661349d6ebe646
T S Shriram <tsshriram@live.in>
2014-07-04 17:23:37 +0530

    FInal commit Day 3

--- lib/rectangle.rb
+++ lib/rectangle.rb
@@ -5,16 +5,20 @@ class Rectangle
     @coordinate2 = coordinate2
 
   end
-  
-  def perimeter
+
+  def collaborating_lines
     line1 = Line.new(Coordinate.new(@coordinate1.x,@coordinate1.y),Coordinate.new(@coordinate1.x,@coordinate2.y))
     line2 = Line.new(Coordinate.new(@coordinate1.x,@coordinate2.y),Coordinate.new(@coordinate2.x,@coordinate2.y))
-    ( line1.length+line2.length ) * 2
+    [line1,line2]
+  end
+
+  def perimeter
+    lines = collaborating_lines
+    ( lines[0].length+lines[1].length ) * 2
   end
 
   def area
-    line1 = Line.new(Coordinate.new(@coordinate1.x,@coordinate1.y),Coordinate.new(@coordinate1.x,@coordinate2.y))
-    line2 = Line.new(Coordinate.new(@coordinate1.x,@coordinate2.y),Coordinate.new(@coordinate2.x,@coordinate2.y))
-     line1.length*line2.length
+    lines = collaborating_lines
+    lines[0].length*lines[1].length
   end
 end
