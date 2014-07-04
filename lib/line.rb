b482ac8658e8801195154983d09bae3f78f5fbec
tshriram02 <TSShriram@TSs-MacBook-Pro.local>
2014-07-04 11:59:16 +0530

    Modifying Line class according to coordinates

--- lib/line.rb
+++ lib/line.rb
@@ -1,20 +1,17 @@
 #Models a line and returns the length
 class Line
-  def initialize x1, y1, x2, y2
-    @x1=x1
-    @y1=y1
-    @x2=x2
-    @y2=y2
+  def initialize coordinate1, coordinate2
+    @coordinate1 = coordinate1
+    @coordinate2 = coordinate2
   end
-
   def coordinates
-    [@x1,@y1,@x2,@y2]
+    [@coordinate1,@coordinate2]
   end
   def length
-    ( Math.sqrt ( @x2 - @x1 )**2 + ( @y2 - @y1 )**2 ).to_i
+    ( Math.sqrt ( @coordinate2.x - @coordinate1.x )**2 + ( @coordinate2.y - @coordinate1.y)**2 ).to_i
   end
 
   def == line2
-    [ @x1,@y1,@x2,@y2 ] == line2.coordinates || [ @x2,@y2,@x1,@y1 ] == line2.coordinates
+    [ @coordinate1, @coordinate2 ] == line2.coordinates || [ @coordinate2, @coordinate1 ] == line2.coordinates
   end
 end
--- spec/line_spec.rb
+++ spec/line_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe Line do
   context "should have a length" do
     it "length should be 1 for the line (1,1),(2,1)" do
-      line = Line.new(1, 1, 2, 1)
+      line = Line.new(Coordinate.new(1, 1), Coordinate.new(2, 1))
       expect(line.length).to eq(1)
     end
 
     it "length should be 5 for the line (1,2),(4,6)" do
-      line = Line.new(1, 2, 4, 6)
+      line = Line.new(Coordinate.new(1, 2), Coordinate.new(4, 6))
       expect(line.length).to eq(5)
     end
 
     it "length should be 8 for the line (2,6),(9,11)" do
-      line = Line.new(2, 6, 9, 11)
+      line = Line.new(Coordinate.new(2, 6), Coordinate.new(9, 11))
       expect(line.length).to eq(8)
     end
 
   end
   context "length should be 0 " do
     it "for lines with same coordinates (2,2) and (2,2)" do
-      line = Line.new(2, 2, 2, 2)
+      line = Line.new(Coordinate.new(2, 2), Coordinate.new(2, 2))
       expect(line.length).to eq(0)
     end
   end
@@ -28,20 +28,20 @@ describe Line do
   context "equality" do
 
     it "lines (1,1),(2,4) and (1,1),(2,4) should be equal" do
-      line1 = Line.new(1, 1, 2, 4)
-      line2 = Line.new(1, 1, 2, 4)
+      line1 = Line.new(Coordinate.new(1, 1), Coordinate.new(2, 4))
+      line2 = Line.new(Coordinate.new(1, 1), Coordinate.new(2, 4))
       expect(line1).to eq(line2)
     end
 
     it "lines (1,8),(3,5) and (2,3),(9,9) should not be equal" do
-      line1 = Line.new(1, 8, 3, 5)
-      line2 = Line.new(2, 3, 9, 9)
+      line1 = Line.new(Coordinate.new(1, 8), Coordinate.new(3, 5))
+      line2 = Line.new(Coordinate.new(2, 3), Coordinate.new(9, 9))
       expect(line1).to_not eq(line2)
     end
 
     it "lines (2,4),(7,8) and (7,8),(2,4) should be equal" do
-      line1 = Line.new(2, 4, 7, 8)
-      line2 = Line.new(7, 8, 2, 4)
+      line1 = Line.new(Coordinate.new(2, 4), Coordinate.new(7, 8))
+      line2 = Line.new(Coordinate.new(7, 8), Coordinate.new(2, 4))
       expect(line1).to eq(line2)
     end
 
